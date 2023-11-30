d3204cbdc13d1943f1f0bc084927f88bd168ee5c
shiva13 <86532158+ssshiva1333@users.noreply.github.com>
2023-11-30 23:21:41 +0300

    Add files via upload

---
+++ static_ip_tool.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const (
+	iface_file  = "/etc/network/interfaces"
+	user_manual = "* parameters -> iface_name inet(4/6) ip_address netmask gateway dns_server\n"
+)
+
+func main() {
+	fmt.Println(user_manual)
+
+	var iface_name, inet, ip_address, netmask, gateway, dns_server string
+	fmt.Print("> ")
+	fmt.Scanln(&iface_name, &inet, &ip_address, &gateway, &netmask, &dns_server)
+
+	write_ip_addr(iface_name, inet, ip_address, netmask, gateway, dns_server)
+}
+
+func write_ip_addr(iface_name, inet, ip_addr, netmask, gateway, dns_servers string) {
+	content := "\niface " + iface_name + " " + inet + "\n"
+	content += "address " + ip_addr + "\n"
+	content += "netmask " + netmask + "\n"
+	content += "gateway " + gateway + "\n"
+	content += "dns_nameservers " + dns_servers
+
+	file, file_error := os.OpenFile(iface_file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if file_error != nil {
+		fmt.Println("* ", file_error.Error())
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	_, writing_error := writer.WriteString(content)
+	if writing_error != nil {
+		fmt.Println("* ", writing_error.Error())
+	}
+
+	flushing_error := writer.Flush()
+	if flushing_error != nil {
+		fmt.Println("* ", flushing_error.Error())
+	}
+}
